46dc3c9048b374dfbeaad7ad313b327808019520
Hendra Gunawan <hengun.bless@gmail.com>
2013-01-24 16:34:08 +0700

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleSlice::Application.config.secret_token = 'dbd0d6d5088e15240473a5273640ba772fd35afc65ac956e3393582f512ff4bfc93c7e350461248f67e514bd7a511bd0938fd545fd85b75acf81e53a7eefc5c4'
